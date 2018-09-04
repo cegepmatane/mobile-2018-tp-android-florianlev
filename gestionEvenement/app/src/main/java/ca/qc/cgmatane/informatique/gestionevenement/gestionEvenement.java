c73ea1a3cd759a19fba19146adb7bda4e176d0b8
darkfloflo <flolevy33@gmail.com>
2018-09-04 06:37:15 +0200

    creation fonction prepareListeEvenements

--- gestionEvenement/app/src/main/java/ca/qc/cgmatane/informatique/gestionevenement/gestionEvenement.java
+++ gestionEvenement/app/src/main/java/ca/qc/cgmatane/informatique/gestionevenement/gestionEvenement.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -18,7 +19,34 @@ public class gestionEvenement extends AppCompatActivity {
         setContentView(R.layout.vue_gestion_evenement);
 
         vueListeEvenement = (ListView) findViewById(R.id.vue_liste_evenement);
+        listeEvenement = prepareListeEvenements();
 
+    }
+
+    public List<HashMap<String,String>> prepareListeEvenements()
+    {
+
+        List<HashMap<String,String>> listeEvenements =
+                new ArrayList<HashMap<String,String>>();
+
+        HashMap<String,String> evenement;
+
+        evenement = new HashMap<String,String>();
+        evenement.put("titre", "reunion");
+        evenement.put("lieu", "cegep Matane");
+        listeEvenements.add(evenement);
+
+        evenement = new HashMap<String,String>();
+        evenement.put("titre", "party");
+        evenement.put("lieu", "chez moi");
+        listeEvenements.add(evenement);
+
+        evenement = new HashMap<String,String>();
+        evenement.put("titre", "recuperer voiture");
+        evenement.put("lieu", "chez keke");
+        listeEvenements.add(evenement);
+
+        return listeEvenements;
     }
 }
 
