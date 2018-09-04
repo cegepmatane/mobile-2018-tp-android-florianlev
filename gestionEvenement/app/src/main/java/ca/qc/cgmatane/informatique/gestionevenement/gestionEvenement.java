4741855c47fa550390c4a5fa73cd92cae8b0f1d7
darkfloflo <flolevy33@gmail.com>
2018-09-04 07:16:42 +0200

    layout modifier evenement

--- gestionEvenement/app/src/main/java/ca/qc/cgmatane/informatique/gestionevenement/gestionEvenement.java
+++ gestionEvenement/app/src/main/java/ca/qc/cgmatane/informatique/gestionevenement/gestionEvenement.java
@@ -72,6 +72,7 @@ public class GestionEvenement extends AppCompatActivity {
                             */
                         message.show();
 
+
                     }}
         );
     }
---
+++ gestionEvenement/app/src/main/java/ca/qc/cgmatane/informatique/gestionevenement/vue/ModifierEvenement.java
@@ -0,0 +1,15 @@
+package ca.qc.cgmatane.informatique.gestionevenement.vue;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import ca.qc.cgmatane.informatique.gestionevenement.R;
+
+public class ModifierEvenement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vue_modifier_evenement);
+    }
+}
