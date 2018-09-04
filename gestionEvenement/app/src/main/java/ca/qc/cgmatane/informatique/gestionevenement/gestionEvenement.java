a8708a108c6fa504a02cb704e6eb9e0e75b5331e
darkfloflo <flolevy33@gmail.com>
2018-09-04 05:51:02 +0200

    Initialisation projet

---
+++ gestionEvenement/app/src/main/java/ca/qc/cgmatane/informatique/gestionevenement/gestionEvenement.java
@@ -0,0 +1,13 @@
+package ca.qc.cgmatane.informatique.gestionevenement;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class gestionEvenement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vue_gestion_projet);
+    }
+}
