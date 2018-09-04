c87c22655ea4efd5fddc3e8af7ce76071cc2622c
darkfloflo <flolevy33@gmail.com>
2018-09-04 06:17:40 +0200

    Création des listes evenement

--- gestionEvenement/app/src/main/java/ca/qc/cgmatane/informatique/gestionevenement/gestionEvenement.java
+++ gestionEvenement/app/src/main/java/ca/qc/cgmatane/informatique/gestionevenement/gestionEvenement.java
@@ -2,12 +2,21 @@ package ca.qc.cgmatane.informatique.gestionevenement;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.HashMap;
+import java.util.List;
 
 public class gestionEvenement extends AppCompatActivity {
+
+    protected ListView vueListeEvenement;
+    protected List<HashMap<String, String>> listeEvenement;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.vue_gestion_projet);
+        setContentView(R.layout.vue_gestion_evenement);
     }
 }
+
+
