68b7e50b1da2591735ee149961f71c195bf1ab0f
darkfloflo <flolevy33@gmail.com>
2018-09-08 15:08:08 -0400

    Création classe BaseDeDonnees et constructeur

---
+++ gestionEvenement/app/src/main/java/ca/qc/cgmatane/informatique/gestionevenement/modele/BaseDeDonnees.java
@@ -0,0 +1,32 @@
+package ca.qc.cgmatane.informatique.gestionevenement.modele;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class BaseDeDonnees extends SQLiteOpenHelper {
+
+    private static BaseDeDonnees instance = null;
+
+    public static BaseDeDonnees getInstance(Context contexte)
+    {
+        instance = new BaseDeDonnees(contexte);
+        return instance;
+    }
+
+    public static BaseDeDonnees getInstance()
+    {
+        return instance;
+    }
+
+    public BaseDeDonnees(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    public BaseDeDonnees(Context contexte) {
+        super(contexte, "gestionEvenement", null, 1);
+    }
+
+
+
+}
