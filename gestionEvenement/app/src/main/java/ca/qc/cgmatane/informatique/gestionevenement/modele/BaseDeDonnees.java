4eed32830a97b248c41b3f03b99e5f2d5002c359
darkfloflo <flolevy33@gmail.com>
2018-09-11 00:58:59 -0400

    insert evenement

--- gestionEvenement/app/src/main/java/ca/qc/cgmatane/informatique/gestionevenement/modele/BaseDeDonnees.java
+++ gestionEvenement/app/src/main/java/ca/qc/cgmatane/informatique/gestionevenement/modele/BaseDeDonnees.java
@@ -28,15 +28,26 @@ public class BaseDeDonnees extends SQLiteOpenHelper {
     }
 
     public void onCreate(SQLiteDatabase db) {
-        String CREATE_TABLE = "create table livre(id_livre INTEGER PRIMARY KEY, titre TEXT, auteur TEXT)";
+        String CREATE_TABLE = "create table evenement(id_evenement INTEGER PRIMARY KEY, titre TEXT, lieu TEXT)";
         db.execSQL(CREATE_TABLE);
     }
 
     public void onOpen(SQLiteDatabase db) {
+        String DELETE = "delete from evenement where 1 = 1";
+        db.execSQL(DELETE);
 
+        String INSERT_1 = "insert into evenement(titre, lieu) VALUES('Faire le menage', 'maison')";
+        String INSERT_2 = "insert into evenement(titre, lieu) VALUES('fete', 'chez gege')";
+        String INSERT_3 = "insert into evenement(titre, lieu) VALUES('haha', 'lolo')";
+
+        db.execSQL(INSERT_1);
+        db.execSQL(INSERT_2);
+        db.execSQL(INSERT_3);
 
     }
 
     public void onUpgrade(SQLiteDatabase db, int arg1, int arg2) {
+        String CREER_TABLE = "create table evenement(id_evenement INTEGER PRIMARY KEY, titre TEXT, lieu TEXT)";
+        db.execSQL(CREER_TABLE);
     }
 }
