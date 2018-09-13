339ef326c683b58f0bd474254b29044fcd19b44e
darkfloflo <flolevy33@gmail.com>
2018-09-12 22:58:16 -0400

    ajout champ date dans vue ajouter

--- gestionEvenement/app/src/main/java/ca/qc/cgmatane/informatique/gestionevenement/vue/ajouterEvenement.java
+++ gestionEvenement/app/src/main/java/ca/qc/cgmatane/informatique/gestionevenement/vue/ajouterEvenement.java
@@ -1,20 +1,26 @@
 package ca.qc.cgmatane.informatique.gestionevenement.vue;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.Calendar;
 
 import ca.qc.cgmatane.informatique.gestionevenement.R;
 import ca.qc.cgmatane.informatique.gestionevenement.donnee.EvenementDao;
 import ca.qc.cgmatane.informatique.gestionevenement.modele.Evenement;
 
 
-public class AjouterEvenement extends AppCompatActivity {
+public class AjouterEvenement extends AppCompatActivity implements DatePickerDialog.OnDateSetListener{
     protected EditText champTitre;
     protected EditText champLieu;
+    protected EditText champDate;
+    protected Calendar dateActuel;
     protected Evenement evenement;
 
     protected EvenementDao accesseurEvenement = EvenementDao.getInstance();
@@ -37,6 +43,25 @@ public class AjouterEvenement extends AppCompatActivity {
                     }
                 }
         );
+
+        champDate = (EditText)findViewById(R.id.vue_ajouter_evenement_date);
+
+        champDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View vue) {
+                dateActuel = Calendar.getInstance();
+                int annee = dateActuel.get(Calendar.YEAR);
+                int mois = dateActuel.get(Calendar.MONTH);
+                int jour = dateActuel.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog dialogueChoixDate = new DatePickerDialog(getApplicationContext(), new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int selectionAnnee, int selectionMois, int selectionJour) {
+                        champDate.setText();
+                    }
+                }, annee, mois, jour);
+                dialogueChoixDate.show();
+            }
+        });
     }
 
     private void enregistrerEvenement()
@@ -59,4 +84,5 @@ public class AjouterEvenement extends AppCompatActivity {
     {
         this.finish();
     }
+
 }
