a24746e036df93a4292442fc130b6610ab6277ab
darkfloflo <flolevy33@gmail.com>
2018-09-13 03:44:16 -0400

    notification fonctionne

--- gestionEvenement/app/src/main/java/ca/qc/cgmatane/informatique/gestionevenement/vue/ajouterEvenement.java
+++ gestionEvenement/app/src/main/java/ca/qc/cgmatane/informatique/gestionevenement/vue/ajouterEvenement.java
@@ -1,8 +1,13 @@
 package ca.qc.cgmatane.informatique.gestionevenement.vue;
 
+import android.app.AlarmManager;
 import android.app.DatePickerDialog;
+import android.app.PendingIntent;
 import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.AlarmManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +18,7 @@ import android.widget.TimePicker;
 import android.text.format.DateFormat;
 import java.util.Calendar;
 
+import ca.qc.cgmatane.informatique.gestionevenement.AlertReceiver;
 import ca.qc.cgmatane.informatique.gestionevenement.R;
 import ca.qc.cgmatane.informatique.gestionevenement.donnee.EvenementDao;
 import ca.qc.cgmatane.informatique.gestionevenement.modele.Evenement;
@@ -65,8 +71,9 @@ public class AjouterEvenement extends AppCompatActivity {
 
                             @Override
                             public void onTimeSet(TimePicker vue, int selectionHeure, int selectionMinute) {
-                                champDate.setText(selectionJour+"-"+selectionMois+"-"+selectionAnnee + "-" + selectionHeure + "H:" + selectionMinute +"mn");
+                                champDate.setText(selectionJour+"-"+selectionMois+"-"+selectionAnnee + " " + selectionHeure + "h:" + selectionMinute +"mn");
                                 dateActuel.set(selectionAnnee,selectionMois,selectionJour,heure,minute);
+                                demarerAlarme(dateActuel);
 
                             }
                         }, heure, minute, DateFormat.is24HourFormat(AjouterEvenement.this));
@@ -76,6 +83,17 @@ public class AjouterEvenement extends AppCompatActivity {
                 dialogueChoixDate.show();
             }
         });
+    }
+
+    private void demarerAlarme(Calendar c){
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+        Intent intent = new Intent(this, AlertReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 1, intent, 0);
+
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, c.getTimeInMillis(), pendingIntent);
+
+
     }
 
     private void enregistrerEvenement()
